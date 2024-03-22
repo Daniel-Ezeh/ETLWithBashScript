1ac0c3ea0bc914d5556155ad15f3608f09d68620
Daniel Ezeh <daniel.ezeh@nomba.com>
2024-03-22 17:30:50 +0100

    yeah

--- initialing_setup.sh
+++ initialing_setup.sh
@@ -8,6 +8,6 @@ echo "Temperature, Humidity, Presure" > logs/Port_weather.log
 # * * * * * /bin/bash  /Users/nombauser/Desktop/GIT/my_git_repos/ETLWithBashScript/temperature_ETL.sh
 
 
-echo "Temperature, Humidity, Presure" > buffered_log/Lag_weather.log
-echo "Temperature, Humidity, Presure" > buffered_log/Abj_weather.log
-echo "Temperature, Humidity, Presure" > buffered_log/Port_weather.log
+echo "Temperature, Humidity, Presure" > buffered_log/Lag_weather.csv
+echo "Temperature, Humidity, Presure" > buffered_log/Abj_weather.csv
+echo "Temperature, Humidity, Presure" > buffered_log/Port_weather.csv
--- temperature_ETL.sh
+++ temperature_ETL.sh
@@ -8,15 +8,15 @@ python3 fetching_data.py | grep "Port Harcourt" | cut -d "," -f2-4 | tr ")" " " 
 
 
 # Buffer the last hour of readings
-head -n1 < logs/Abj_weather.log > buffered_log/Abj_weather.log && tail -n10 <  logs/Abj_weather.log >> buffered_log/Abj_weather.log
-head -n1 < logs/Lag_weather.log > buffered_log/Lag_weather.log && tail -n10 <  logs/Lag_weather.log >> buffered_log/Lag_weather.log
-head -n1 < logs/Port_weather.log > buffered_log/Port_weather.log && tail -n10 <  logs/Port_weather.log >> buffered_log/Port_weather.log
+head -n1 < logs/Abj_weather.log > buffered_log/Abj_weather.csv && tail -n10 <  logs/Abj_weather.log >> buffered_log/Abj_weather.csv
+head -n1 < logs/Lag_weather.log > buffered_log/Lag_weather.csv && tail -n10 <  logs/Lag_weather.log >> buffered_log/Lag_weather.csv
+head -n1 < logs/Port_weather.log > buffered_log/Port_weather.csv && tail -n10 <  logs/Port_weather.log >> buffered_log/Port_weather.csv
 
 sleep 1
 
-cat <  buffered_log/Abj_weather.log > logs/Abj_weather.log
-cat <  buffered_log/Lag_weather.log > logs/Lag_weather.log
-cat <  buffered_log/Port_weather.log > logs/Port_weather.log
+cat <  buffered_log/Abj_weather.csv > logs/Abj_weather.log
+cat <  buffered_log/Lag_weather.csv > logs/Lag_weather.log
+cat <  buffered_log/Port_weather.csv > logs/Port_weather.log
 
 
 
