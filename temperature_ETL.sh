280147b66ce72101bbc8cd634248694c647f1822
Daniel Ezeh <daniel.ezeh@nomba.com>
2024-03-22 11:49:36 +0100

    Added .gitignore and temperature.log file

---
+++ temperature_ETL.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Extract reading with get_temp_API
+
+# Append reading to temperature.log
+
+# Buffer the last hour of readings
+
+# Call get_stats.py to  aggregate the readings.
+
+# Load the stats using load_stats_api
+
+# Schedule the script to run every minutes.
